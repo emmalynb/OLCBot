efba9e6108f8fb781719e89f9f814023aeb82eff
Brady Pascoe <bpas247@hotmail.com>
2018-10-10 13:27:26 -0700

    Potential fix for !alive

--- bot.js
+++ bot.js
@@ -76,9 +76,15 @@ client.on("message", async message => {
 
       const days = Math.floor(difference / (1000*60*60*24));
 
+      difference -= difference * 100 * 60 * 60 * 24;
+
       const hours = Math.floor(difference / (1000*60*60));
+
+      difference -= hours * 1000 * 60 * 60;
       
       const minutes = Math.floor(difference / (1000*60));
+
+      difference -= minutes * 1000 * 60;
 
       const seconds = Math.floor(difference / (1000));
 
